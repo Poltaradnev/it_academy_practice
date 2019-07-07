f1fe094cf753bc330c421fbc9c8a23b857521aa9
Poltaradnev <poltaradnev@mail.ru>
2019-07-07 21:25:34 +0300

    classwork1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan  date1 = DateTime.Now.TimeOfDay;
+            Int32 hour1 = date1.Hours;
+
+            if (hour1 >= 9 && hour1 < 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (hour1 >= 12 && hour1 < 15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (hour1 >= 15 && hour1 < 22)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+        }
+    }
+}         
