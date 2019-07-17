8c8f952d588277e0bf49cba08cf55af7ae9126fc
Poltaradnev <poltaradnev@mail.ru>
2019-07-17 22:09:58 +0300

    lecture_8

---
+++ task2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] varArray = { "32", "A", "Hello" };
+            GetArr(varArray);
+            int tempInt = Convert.ToInt32(varArray[0]) + 10;
+            string tempString = varArray[2] + ",guys!";
+            varArray[2] = tempString;
+            varArray[0] = Convert.ToString(tempInt);
+            GetArr(varArray); 
+        }
+        static void GetArr(string[] tempArray)
+        {
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Console.Write($"{tempArray[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ task3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ranArray = new int[13];
+            int tempNum = 0;
+
+            Random ranNum = new Random();
+            for (int i = 0; i < ranArray.Length; i++)
+            {
+                ranArray[i] = ranNum.Next(600);
+            }
+
+            for (int i = 0; i < ranArray.Length; i++)
+            {
+                if (tempNum < ranArray[i])
+                    tempNum = ranArray[i];
+            }
+
+            for (int i = 0; i < ranArray.Length; i++)
+            {
+                Console.Write($"{ranArray[i]}\n");
+            }
+            Console.WriteLine($"\nMaximum array value = {tempNum}");
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
